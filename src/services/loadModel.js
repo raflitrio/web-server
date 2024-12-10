1ca7456906b802b909453d07faee9f2b328d9a01
Rafli Triofansyah <151233287+raflitrio@users.noreply.github.com>
2024-12-10 14:23:00 +0700

    Delete src directory

--- src/services/loadModel.js
+++
@@ -1,8 +0,0 @@
-const tf = require('@tensorflow/tfjs');  // Import TensorFlow.js
-require('dotenv').config();  // Import dotenv if you're using .env files
-
-async function loadModel() {
-    return tf.loadGraphModel(process.env.MODEL_URL); // Load the model using the URL in the environment variable
-}
-
-module.exports = loadModel;
